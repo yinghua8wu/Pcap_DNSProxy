24b95241f3bb01816137c076e5aca40cafebdf07
yinghua8wu <38562798+yinghua8wu@users.noreply.github.com>
2020-05-21 20:18:17 +0800

    Update ubuntu1804_install.sh

--- ubuntu1804_install.sh
+++ ubuntu1804_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install build-essential cmake libevent-dev libpcap-dev libsodium-dev openssl libssl-dev git dnsutils -y
+sudo apt install build-essential cmake libevent-dev libpcap-dev libsodium-dev openssl libssl-dev git dnsutils nscd -y
 
 sudo git clone https://github.com/yinghua8wu/Pcap_DNSProxy.git
 cd Pcap_DNSProxy/Source/Auxiliary/Scripts
@@ -14,3 +14,5 @@ sudo service systemd-resolved stop
 
 cd /usr/local/Pcap_DNSProxy
 sudo ./Linux_Install.Systemd.sh
+
+echo "nameserver 127.0.0.1" > /etc/resolv.conf && /etc/init.d/nscd restart
